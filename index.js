5115d77a0aefa21d515a17e962382ddeaeab9074
ashutoshumar <viraj28300@gmail.com>
2022-05-13 01:23:05 +0530

    make it better as wow uff huff duff

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use("/comment",commentRoute);
 app.use("/categories",categoryRoute);
 
 
-app.listen(proces.env.PORT || '5000',()=>{
+app.listen(process.env.PORT || '5000',()=>{
     console.log("connected to the server")
 
 })
